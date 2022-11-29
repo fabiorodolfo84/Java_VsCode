86a6688b5ac80508a70025f5c1658b59b2f73363
fabiorodolfo84 <fabiorodolfo1984@gmail.com>
2022-11-28 23:05:39 -0300

    update

---
+++ Estudos-K19/src/Ex01_If.java
@@ -0,0 +1,16 @@
+public class Ex01_If {
+    //A altura minima para ingresso na atração "The Barnstorner" do parque tematico da 
+    //Disney "Magiv Kingtom é 0.89 metros. Vamos simular a execução do programa
+    //que controla o acesso dos visitances a essa atração."
+
+    public static void main(String[] args) {
+        double altura = Math.random();
+        System.out.println(altura);
+        if(altura < 0.89){
+            System.out.println("Acesso bloqueado");
+        }
+        System.out.println("The Barnstormer");
+
+
+    }
+}
