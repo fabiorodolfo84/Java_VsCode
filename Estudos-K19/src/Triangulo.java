77279673046f2b0863567a74d79badec8761e090
fabiorodolfo84 <fabiorodolfo1984@gmail.com>
2022-11-28 22:13:51 -0300

    update

---
+++ Estudos-K19/src/Triangulo.java
@@ -0,0 +1,12 @@
+public class Triangulo {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");     
+               
+    }
+    
+}
---
+++ Estudos-K19/src/variaveis.java
@@ -0,0 +1,11 @@
+public class variaveis {
+    public static void main(String[] args) {
+        String s1 = "Fabio";
+        String s2 = " ";
+        String s3 = "Rodolfo";
+        String s4 = s1 + s2 + s3;
+
+        System.out.println(s4);
+        
+    }
+}
