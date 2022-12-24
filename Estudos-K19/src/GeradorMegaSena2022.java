da1453ab5a639336054c199785bbc8ce1aed8bab
fabiorodolfo1984@gmail.com <fabiorodolfo1984@gmail.com>
2022-12-24 19:48:08 -0300

    update

---
+++ Estudos-K19/src/GeradorMegaSena2022.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+
+public class GeradorMegaSena2022 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        for(int i = 0; i <6; i++) {
+            int num = random.nextInt(61); // gera um numero aleatorio de 0 e 60.
+
+            System.out.println("Os numeros gerados é: " + num);
+        }
+    }
+
+
+}
+   
