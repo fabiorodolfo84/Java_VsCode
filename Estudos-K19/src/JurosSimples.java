54001d0d476376b04fae2d2860fc8543d0cd28c9
fabiorodolfo1984@gmail.com <fabiorodolfo1984@gmail.com>
2022-12-24 19:47:44 -0300

    update

---
+++ Estudos-K19/src/JurosSimples.java
@@ -0,0 +1,29 @@
+public class JurosSimples {
+    public static void main(String[] args) {
+        double P; // valor principal
+        double J; // taxa de juros
+        int T; // periodo em meses
+
+        //Atribua valores a essas variaveis. Por exemplo:
+
+
+        P = 1000; // valor principal é 1000
+        J = 0.05; // taxa de juros é 5%
+        T = 6; // periodo é de 6 meses
+
+        // agora vamos calcular o valor dos juros usando a formula de juros simples: juros = P*J*T. 
+        //Por exemplo
+
+        double juros = P * J * T;
+
+        // vamos adiocionar os juros ao valor principal para obter o valor total a ser pago
+        // por exemplo:
+
+        double total = P + juros;
+
+    System.out.println("O valor total a ser pargo é: " + total);
+
+        
+
+    }
+}
